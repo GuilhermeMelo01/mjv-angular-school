5f17b3652971e4676e1cb0595ebf4d9d0c0807b7
Guilherme <meloguilherme1994@gmail.com>
2023-03-03 10:24:49 -0300

    aula03 - tipos primitivos

---
+++ mjv-angular-school/javascript/aula03-tipos-primitivos.js
@@ -0,0 +1,14 @@
+let string = "string"; //valores de textos
+
+let number = 1234; //numeros com pontos flutuantes
+ 
+let bigint = 12345n; //numeros muito grandes 
+
+let boolean = true; // true ou false
+
+let symbol = Symbol(); //Um valor que é sempre unico
+
+let undef = undefined; //valores ainda não definidos
+
+let nul = null; //Representa a ausencia de um valor de forma explicita
+
