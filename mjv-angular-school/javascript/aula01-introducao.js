68db02aca6f7443c5d67401c514a3e0bab7d2cb2
Guilherme <meloguilherme1994@gmail.com>
2023-03-03 10:13:37 -0300

    aula-01 introdução javascript

---
+++ mjv-angular-school/javascript/aula01-introducao.js
@@ -0,0 +1,33 @@
+let x = 10;
+
+if(x >= 10){
+    //
+}
+
+// comntarios de uma linha
+
+/**
+ * Comentarios
+ * de
+ * varias
+ * linhas
+ */
+
+/*
+*/
+
+let alan = {
+    nome: "alan"
+}
+
+let nathan = {
+    nome: "nathan"
+}
+
+let pessoas = [alan, nathan];
+
+let nomeAlan = alan.nome;
+let nomeNathan = nathan['nome'];
+
+let pessoaAlan = pessaos[0];
+let pessoaNathan = pessoas[1];
