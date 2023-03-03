264ae4c5c095ed29149e53b0e86e670f81df16fe
Guilherme <meloguilherme1994@gmail.com>
2023-03-03 10:37:33 -0300

    aula05 - estruturas de controle

---
+++ mjv-angular-school/javascript/aula05-estruturas-controles.js
@@ -0,0 +1,26 @@
+const x = 3; 
+
+//If e Else
+if( x >= 3){
+
+}else if(x < 10){
+
+}else{
+
+}
+
+
+//Switch
+switch(x){
+    case 1: {
+        break;
+    }
+    case 2: {
+        break;
+    }
+    case 3: {
+        break;
+    }
+    default: {
+    }
+}
