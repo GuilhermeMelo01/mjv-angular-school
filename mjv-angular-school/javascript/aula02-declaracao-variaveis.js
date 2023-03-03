4d44e9f259de4526aced9d6fd45d4cf32cfbb1c2
Guilherme <meloguilherme1994@gmail.com>
2023-03-03 10:20:12 -0300

    aula02 - declaracao de variaveis

---
+++ mjv-angular-school/javascript/aula02-declaracao-variaveis.js
@@ -0,0 +1,10 @@
+let x; // O let nos permite atribuir um valor posteriomente 
+
+x = 10;
+x = 21;
+
+const y = 2; // Temos que declarar um valor no momento de criação, e não poderemos substitui o valor depois.
+
+y = 10; //error
+
+var a = "a"; // Similar ao let, só que ele é depreciado
