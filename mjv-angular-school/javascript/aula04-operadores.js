e62ed99944f3aab8225725113d94f8fdf9bd5a32
Guilherme <meloguilherme1994@gmail.com>
2023-03-03 10:30:13 -0300

    aula04 - operadores

---
+++ mjv-angular-school/javascript/aula04-operadores.js
@@ -0,0 +1,26 @@
+let x = 10;
+let y = x;
+
+//Operadores numericos
+y = 10 + 10;
+y = 10 - 10;
+y = 10 / 10;
+y = 10 * 10;
+
+//Operadores de atribuicões
+y = y + 5;
+y += 5;
+
+//Operadores booleanos
+// > | < | === ou == | <= | >= | != ou !==
+
+// === e !== tambem valida o tipo da variavel
+if(y > 10){
+    //
+}
+
+//Operadores logicos
+// && - ||
+if(y > 10 && y < 20){
+    //
+}
