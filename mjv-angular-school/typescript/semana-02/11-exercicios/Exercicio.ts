36d385bca0b82e93e2041bb6aa77406b3c254f11
Guilherme <meloguilherme1994@gmail.com>
2023-03-16 11:34:31 -0300

    semana-02 Perguntas do exercicio de typescript

--- mjv-angular-school/typescript/semana-02/11-exercicios/Exercicio.ts
+++ mjv-angular-school/typescript/semana-02/11-exercicios/Exercicio.ts
@@ -1,38 +1,39 @@
-// Exercicio 1:
+// Exercicio 1: Crie um tipo para representar um objeto que contenha as suas informações de nome, profissão, idade e uma lista de assuntos de seu interesse.
 class Myself {
     constructor(public nome:string, public profissao:string, public idade:number, public interesses:string[]){
     }
 }
 
-//Exercicio 2:
+//Exercicio 2: Crie um objeto utilizando o tipo criado no exercicio 1 e o popule de acordo com suas informações.
 const guilherme = new Myself("Guilherme", "Desenvolvedor Java", 21, ["Programação", "CS:GO", "Animes"]);
 
-//Exercicio 3 e 4:
+//Exercicio 3: Faça um função que receba como argumento um objeto do tipo do exercicio 1, e retorne somente a lista de assuntos do objeto.
+//Exercicio 4: Coloque tipagem tanto no argumento da função do exercicio 3 quanto o tipo de retorno dela.
 function getInteresses(user:Myself): string[]{
     return user.interesses;
 }
 
-//Exercicio 5:
+//Exercicio 5: Crie um enum para representar as Matérias do curso (Angular, Typescript, Git)
 enum Materias {
     angular = "Angular",
     typescript = "Typescript",
     git = "Git"
 }
 
-//Exercicio 6:
+//Exercicio 6: Crie mais um tipo para respresentar os professores, contendo nome e uma lista das materias de cada um.
 class Professor{    
     constructor(public nome:string, public materias:Materias[]){
     }
 }
 
-//Exercicio 7:
+//Exercicio 7: Crie os objetos Alan e Nathan utilizando os tipos dos exercicicios 5 e 6.
 const nathan = new Professor("Nathan", [Materias.angular, Materias.git])
 const alan = new Professor("Alan", [Materias.angular, Materias.typescript, Materias.git]);
 
-//Exercicio 8:
+//Exercicio 8: Declare e popule um array com os objetos do exercicio 7.
 const professores: Array<Professor> = [nathan, alan];
 
-//Exercicio 9:
+//Exercicio 9: Faça uma função que receba um argumento de array de Professor e retorne um novo array de strings contendo somente os nomes dos professores.
 function getNomeProfessores(profs:Professor[]): string[]{
     let tempProfsNome: string[] = [];
 
@@ -43,7 +44,7 @@ function getNomeProfessores(profs:Professor[]): string[]{
 }
 console.log(getNomeProfessores([nathan, alan]));
 
-//Exercicio 10:
+//Exercicio 10: Faça uma função que receba um argumento de array de Professores e retorne um array de matérias.
 function getMateriasProfessores(profs: Professor[]): Materias[]{
     let materias: Materias[] = [];
     
@@ -56,7 +57,7 @@ function getMateriasProfessores(profs: Professor[]): Materias[]{
 console.log(getMateriasProfessores([nathan, alan]));
 
 
-//Exercicio 11:
+//Exercicio 11: Faça uma função que receba um argumento de array de Professores e retorne o primeiro Professor encotrado que dê aula de Typesrcipt
 function getProfessorTypescript(profs:Professor[]): Professor{
 
     // return profs.filter(prof => prof.materias.includes(Materias.typescript)).find(p => p.nome)!
