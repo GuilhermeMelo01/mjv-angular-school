1c9231207c4ad8071a105af396a80d13c08d9a20
Guilherme <meloguilherme1994@gmail.com>
2023-03-20 10:19:57 -0300

    Feito a parte do header para o exercicio de colaboradores

---
+++ mjv-angular-school/exercicio-angular-sm-03/src/app/features/colaboradores/components/components.module.ts
@@ -0,0 +1,16 @@
+import { ListagemColaboradoresComponent } from './../listagem-colaboradores/listagem-colaboradores.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    ListagemColaboradoresComponent
+  ]
+})
+export class ComponentsModule { }
