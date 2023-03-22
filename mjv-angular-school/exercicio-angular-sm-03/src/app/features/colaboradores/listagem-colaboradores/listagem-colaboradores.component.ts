9fa9ab5e48def940ec6fce9506b01ec371ebd32c
Guilherme <meloguilherme1994@gmail.com>
2023-03-22 08:25:32 -0300

    Criado objeto e variavel no ts e passado para html

--- mjv-angular-school/exercicio-angular-sm-03/src/app/features/colaboradores/listagem-colaboradores/listagem-colaboradores.component.ts
+++ mjv-angular-school/exercicio-angular-sm-03/src/app/features/colaboradores/listagem-colaboradores/listagem-colaboradores.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-listagem-colaboradores',
   templateUrl: './listagem-colaboradores.component.html',
   styleUrls: ['./listagem-colaboradores.component.scss']
 })
-export class ListagemColaboradoresComponent {
+export class ListagemColaboradoresComponent implements OnInit{
+
+  titulo:string = "COLABORADORES";
+
+  colaborador = {
+    id: 1,
+    nome: "Guilherme",
+    salario: 4500.00,
+    cargo: "Desenvolvedor"
+  }
+
+  ngOnInit(): void {
+  }
 
 }
