80fb80836b379b5d6c23f9c20ad629e767b44ed5
Guilherme <meloguilherme1994@gmail.com>
2023-04-03 11:11:53 -0300

    typescript aula 09 - mapped types

---
+++ mjv-angular-school/typescript/semana-05/09-mapped-types/mapped-types.ts
@@ -0,0 +1,24 @@
+export type MappedType = {
+    [K in  "prop1" | "prop2"]: K 
+}
+
+export type User = {
+    name: string;
+    age: number;
+}
+
+export type MappedUser = MappedGeneric<User>
+
+export type ModifiedUser = {
+    readonly [K in keyof User]?: User[K] | null
+}
+
+export type MappedGeneric<T> = {
+    [K in keyof T]: T[K]
+}
+
+export type MappedGenericCustomProps<T> = {
+    [K in keyof T as `get${Capitalize<Extract<K, string>>}`]: () => T[K]
+}
+
+export type MappedGenericCustomPropsUser = MappedGenericCustomProps<User>
